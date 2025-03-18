aa6d2ca9c94b2e3c70692619247d35edf875115f
wjdansrud <wjdansrud092208@gmail.com>
2025-03-18 09:30:31 +0900

    BOJ_2562

---
+++ BOJ_2503.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/2702
+#include <iostream>
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i  = 0 ; i < n ; i++) {
+        int num, a,b;
+        std::cin >> num >> a >> b;
+
+
+    }
+}
